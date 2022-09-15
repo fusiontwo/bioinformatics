50f532b1572e74ce57fe19e05eb9b93a86161ad1
fusiontwo <101881124+fusiontwo@users.noreply.github.com>
2022-09-15 10:13:26 +0900

    Add files via upload

--- python bioinformatics_4_1.py
+++ python bioinformatics_4_1.py
@@ -1,7 +1,14 @@
-file = open('sequence.protein.gb','r')
+file = open('sequence.protein.gb', 'r')
 title = file.readlines()
-print("title: ",title[0])
+print("title: ", title[0], end='')
+
+file = open('sequence.protein.gb', 'r')
 content = file.readlines()
-seq = content.split('ORIGIN')
-print("seq: ",seq[1])
+content_join = ''.join(content)
+content_divide = content_join.split("ORIGIN")
+seq = content_divide[1]
+seq = seq.lstrip()
+
+print("seq: ", end='')
+print("  ", seq)
 file.close()
