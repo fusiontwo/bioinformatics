5e1be4857f82abcbad646cadd62f52bb3f5d398c
fusiontwo <101881124+fusiontwo@users.noreply.github.com>
2022-09-19 03:20:45 +0900

    Add files via upload

---
+++ bioinformatics_5_1.py
@@ -0,0 +1,17 @@
+def readfile(fasta_file):
+    seqs = []
+    for line in open(fasta_file, 'r'):
+            if line.startswith('>'):
+                header = line.rstrip()
+            else:
+                seq = line.rstrip()
+                seqs.append(seq)
+            sequence = ''.join(seqs)
+    return sequence
+
+sequence = readfile('sequence.nucleotide.fasta')
+
+if __name__ == "__main__":
+    codon = list(map(''.join, zip(*[iter(sequence)]*3)))  # sequence 문자열을 3개씩 잘라서 codon 리스트에 넣는다.
+    for i in range(0, len(codon)):
+        print(codon[i])
