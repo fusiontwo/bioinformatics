70c659081ce2abeb2357d3482fd0dc5306326d9b
fusiontwo <101881124+fusiontwo@users.noreply.github.com>
2022-10-06 17:33:48 +0900

    Add files via upload

---
+++ bioinformatics_3_8_all_a.a..py
@@ -0,0 +1,21 @@
+from bioinformatics_3_6 import load_fasta
+seq_d = load_fasta('sequence.protein.fasta')
+seq = list(seq_d.values())[0]
+
+Amino_acids = ["A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "M", "L", "K", "F", "P", "S", "T", "W", "Y", "V", "*"]
+
+while True:
+    amino_acid = input("Searching for: ")
+    for i in range(21):
+        if amino_acid == Amino_acids[i]:
+            listA = []
+            for pos, char in enumerate(seq, start=1):  # enumerate() 함수는 (인덱스, 원소)로 이루어진 튜플을 만들어준다.
+                if char == Amino_acids[i]:  # 문자열이 'S'와 일치하면
+                    listA.append(pos)  # pos(인덱스)를 출력
+            print("Found at: ", end='')  # 다음 내용을 붙여쓰기 위해서 end=''를 사용
+            for i in listA:
+                print(i, end=',')  # 다음 내용에 ,를 넣어 붙여쓰기 위해서 end=','를 사용
+            print("\n")
+        elif amino_acid == 'XXX':
+            print("Okay, I will stop.")
+            break
