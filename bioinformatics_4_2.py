4ca6fea6c106fc4edd5d40bf548d73a1001eb5be
Reeeeeum <101881124+fusiontwo@users.noreply.github.com>
2022-10-25 10:41:05 +0900

    Delete bioinformatics_4_2.py

--- bioinformatics_4_2.py
+++
@@ -1,6 +0,0 @@
-from bioinformatics_4_1 import seq
-
-seq_str = ''.join([i for i in seq if not i.isdigit()])
-seq_strip1 = seq_str.replace('\n', '')
-seq_strip2 = seq_strip1.replace(' ', '')
-print("seq: ", seq_strip2.replace("//", ""))
